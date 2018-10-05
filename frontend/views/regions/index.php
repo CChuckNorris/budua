94ce5fcd770fd0a794714687f776987735aa8992
CChuckNorris <papanadoma@gmail.com>
2018-10-05 22:28:06 +0300

    - h2 to h1

--- frontend/views/regions/index.php
+++ frontend/views/regions/index.php
@@ -21,7 +21,7 @@ $this->registerMetaTag([
 <div class="pinned-block">
 
     <div class="align-center">
-        <h2 class="section-title lined bottom-offset"><?= $region->h1_title; ?></h2>
+        <h1 class="section-title lined bottom-offset"><?= $region->h1_title; ?></h1>
     </div>
 
     <?= \frontend\components\RatingSortControlsWidget::widget(
