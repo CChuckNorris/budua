acd3b5c08b8ee63f46f8364f851cefb7ba5134fe
CChuckNorris <papanadoma@gmail.com>
2018-10-05 22:57:26 +0300

    - working on regions

--- backend/views/yii2-app/region/_form.php
+++ backend/views/yii2-app/region/_form.php
@@ -1,7 +1,9 @@
 <?php
 
+use dosamigos\ckeditor\CKEditor;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Region */
@@ -14,13 +16,15 @@ use yii\widgets\ActiveForm;
 
     <div class="row">
         <div class="col-md-6">
-            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-md-6">
 
-            <?php $form->field($model, 'alias')->textInput(['maxlength' => true]) ?>
+                    <?= $form->field($model, 'alias')->textInput(['maxlength' => true]) ?>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -51,6 +55,15 @@ use yii\widgets\ActiveForm;
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-md-12">
+            <?= $form->field($model, 'about')->widget(CKEditor::className(), [
+                'options' => ['rows' => 6],
+                'preset' => 'full'
+            ]) ?>
+        </div>
+    </div>
+
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Редактировать', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
@@ -58,3 +71,16 @@ use yii\widgets\ActiveForm;
     <?php ActiveForm::end(); ?>
 
 </div>
+<?php
+$url2 = Url::toRoute(["/slug-translator/get-slug"]);
+?>
+<script>
+
+    $('#region-name').change(function () {
+        var name = $(this).val();
+        $.get("<?= $url2 ?>", {slug: name}, function (data) {
+            data = $.parseJSON(data);
+            $('#region-alias').attr("value", data.alias);
+        });
+    });
+</script>
