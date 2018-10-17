44727657a658fa4a3c2aa81a9bf3bc5d54a12b9b
CChuckNorris <papanadoma@gmail.com>
2018-10-17 22:54:27 +0300

    - fixes

--- frontend/views/layouts/partials/_top_site_bar.php
+++ frontend/views/layouts/partials/_top_site_bar.php
@@ -1,12 +1,12 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-xs-4 col-md-2">
+            <div class="col-md-2">
                 <a href="<?= Yii::$app->homeUrl; ?>" class="logo-header">
                     <img src="<?= \yii\helpers\Url::to("/img/logo-header.png")?>" alt="Seo-stars">
                 </a>
             </div>
-            <div id="top-menu-holder" class="col-xs-8 end-xs col-md-9 start-md">
+            <div id="top-menu-holder" class="col-md-9">
 
                 <?= $this->render("_menu"); ?>
 
